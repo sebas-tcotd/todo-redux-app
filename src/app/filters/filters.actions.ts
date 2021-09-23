d3f8bfc8c373b4962fb0f140400e44bdf974bc2f
Sebastian Vargas <vargaspizango1@gmail.com>
2021-09-22 23:27:47 -0500

    Auto-generated commit [ci skip]

--- src/app/filters/filters.actions.ts
+++
@@ -1,8 +0,0 @@
-import { createAction, props } from '@ngrx/store';
-
-export type validFilters = 'todos' | 'completed' | 'pendings';
-
-export const setFilter = createAction(
-  '[Filter] Set Filter',
-  props<{ filter: validFilters }>()
-);
